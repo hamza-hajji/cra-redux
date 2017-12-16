b69196dacb6336bc880efd8b597ddd5e1b5c93b1
Hamza Hajji <hmzhjji@gmail.com>
2017-12-16 18:31:16 +0000

    fix issue with sample reducer

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -2,7 +2,10 @@ import { SET_VALUE } from "../constants";
 
 export default (state = {}, action) => {
   if (action.type === SET_VALUE) {
-    return {value: action.value}
+    return {
+      ...state,
+      value: action.value
+    };
   }
 
   return state;
